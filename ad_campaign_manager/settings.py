cbaf9b43597a90b07aad18741af467feb5360d47
Regina0101 <ritodo1010@gmail.com>
2024-09-26 15:06:11 +0100

    changees

--- ad_campaign_manager/settings.py
+++ ad_campaign_manager/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['34.88.122.31', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 
